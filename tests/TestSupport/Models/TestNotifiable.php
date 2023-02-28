21a9aa5d7ebd93467222dd98ab706605199d6e59
sfolador <sfolador@gmail.com>
2023-02-28 20:49:48 +0100

    added pushnotifications

--- tests/TestSupport/Models/TestNotifiable.php
+++ tests/TestSupport/Models/TestNotifiable.php
@@ -4,12 +4,14 @@ namespace Sfolador\Devices\Tests\TestSupport\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as BaseUser;
+use Illuminate\Notifications\RoutesNotifications;
 use Sfolador\Devices\Models\Concerns\HasDevices;
 
 class TestNotifiable extends BaseUser
 {
     use HasFactory;
     use HasDevices;
+    use RoutesNotifications;
 
     protected $table = 'testnotifiables';
 
