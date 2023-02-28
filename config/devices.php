bfe0b7a5b391fef9bb1b42c94052b1e59eb19dce
Simone Folador <sfolador@gmail.com>
2023-02-28 15:01:56 +0100

    Update devices.php

--- config/devices.php
+++ config/devices.php
@@ -2,6 +2,10 @@
 
 return [
 
+     /*
+     * If true, it will be possible to attach the same device to another user, this happens 
+     * usually for mobile applications with multi-users
+     */
     'allow_device_reassign' => false,
 
 ];
